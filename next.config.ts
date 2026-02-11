e681ba65d1c3c4b2866099c3b5c69b8acbbc2fb0
Engineer-BK <bachirak47@gmail.com>
2026-02-12 00:01:17 +0530

    Add proxy rewrites configuration

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_USER_SERVICE}/api/v1/:path*`,
-      },
-      {
-        source: "/api/chat/:path*",
-        destination: `${process.env.NEXT_PUBLIC_CHAT_SERVICE}/api/v1/:path*`,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
